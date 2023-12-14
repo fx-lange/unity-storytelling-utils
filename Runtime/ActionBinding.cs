35adfa6f8eaa0a6e24d31354689b4b83577d07c1
Felix Lange <mail@felixlange.dev>
2023-12-14 17:04:49 +0100

    action binding for input system to quickly hock up actions in the inspector

---
+++ Runtime/ActionBinding.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class ActionBinder : MonoBehaviour
+{
+    [Serializable]
+    public class Binding
+    {
+        public string name;
+        public InputActionReference actionReference;
+        public UnityEvent @event;
+        public bool continues = false;
+    }
+        
+    [SerializeField] private List<Binding> actionBindings;
+
+    private void Start()
+    {
+        foreach (var binding in actionBindings)
+        {
+            var action = binding.actionReference.action;
+            if (!binding.continues)
+            {
+                action.performed += _ => binding.@event.Invoke();
+            }
+                
+            action.Enable();
+            action.actionMap.Enable();
+            binding.actionReference.asset.Enable();
+        }
+    }
+
+    private void Update()
+    {
+        foreach (var binding in actionBindings)
+        {
+            if (binding.continues)
+            {
+                if (binding.actionReference.action.IsPressed())
+                {
+                    binding.@event.Invoke();
+                }
+            }
+        }
+    }
+}
